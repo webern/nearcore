9476742912b36897eb431129f1fd589992847693
Piotr Mikulski <piotr@near.org>
2021-12-03 11:13:45 -0800

    Add tests

--- chain/network/src/routing/edge_set.rs
+++ chain/network/src/routing/edge_set.rs
@@ -59,3 +59,54 @@ impl EdgeSet {
         self.repr.len()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::routing::edge_set::EdgeSet;
+    use crate::routing::Edge;
+    use near_primitives::network::PeerId;
+
+    #[test]
+    fn test_remove_key() {
+        let p1 = PeerId::random();
+        let p2 = PeerId::random();
+        let p3 = PeerId::random();
+        let p4 = PeerId::random();
+        let e1 = Edge::make_fake_edge(p1, p2, 1);
+        let e2 = Edge::make_fake_edge(p3, p4, 1);
+        let mut se = EdgeSet::default();
+        se.insert(e2.clone());
+
+        let key = e1.key().clone();
+        se.insert(e1.clone());
+        assert_eq!(se.get(&key).unwrap(), &e1);
+        se.remove(e1.key());
+        assert_eq!(se.get(&key), None);
+
+        let key2 = e2.key().clone();
+        assert_eq!(se.get(&key2).unwrap(), &e2);
+    }
+
+    #[test]
+    fn test_hashset() {
+        let p1 = PeerId::random();
+        let p2 = PeerId::random();
+        let p3 = PeerId::random();
+        let e0 = Edge::make_fake_edge(p1.clone(), p3, 1);
+        let e1 = Edge::make_fake_edge(p1.clone(), p2.clone(), 1);
+        let e2 = Edge::make_fake_edge(p1.clone(), p2.clone(), 2);
+        let e3 = Edge::make_fake_edge(p2, p1, 3);
+
+        let mut se = EdgeSet::default();
+        se.insert(e0.clone());
+        se.insert(e1);
+        se.insert(e2);
+        se.insert(e3.clone());
+
+        let key3 = e3.key().clone();
+        let key0 = e0.key().clone();
+
+        assert_eq!(se.get(&key3).unwrap(), &e3);
+        assert_eq!(se.get(&key0).unwrap(), &e0);
+    }
+}
